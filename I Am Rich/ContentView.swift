6e0d20c5feb7bafd0374f9f3d7d3dc351851afd8
Tim Newton <tim.newton@eduserve.us>
2019-11-25 16:21:41 -0800

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Tim Newton on 11/25/19.
+//  Copyright © 2019 EduServe, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
